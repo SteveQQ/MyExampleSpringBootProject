fb16782bd58df40c196cb2cd398241e5f7ecc3f0
SteveQQ <sawicki.adam172@gmail.com>
2017-04-27 20:58:33 +0200

    Initial commit

---
+++ src/main/java/com/steveq/MyExampleSpringBootProjectApplication.java
@@ -0,0 +1,12 @@
+package com.steveq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyExampleSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyExampleSpringBootProjectApplication.class, args);
+	}
+}
