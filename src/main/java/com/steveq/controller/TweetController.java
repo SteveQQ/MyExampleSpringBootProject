772c1bf0e34de73b029e911f48d9ed899a796444
SteveQQ <sawicki.adam172@gmail.com>
2017-05-11 20:40:48 +0200

    Basic search and result sites

---
+++ src/main/java/com/steveq/controller/TweetController.java
@@ -0,0 +1,56 @@
+package com.steveq.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.social.twitter.api.SearchResults;
+import org.springframework.social.twitter.api.Tweet;
+import org.springframework.social.twitter.api.Twitter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.codehaus.groovy.runtime.DefaultGroovyMethods.collect;
+
+/**
+ * Created by Adam on 2017-04-27.
+ */
+@Controller
+@PropertySource(value = "secrets.properties")
+public class TweetController {
+    @Autowired
+    private Twitter twitter;
+
+    @RequestMapping(value = "/")
+    public String home(){
+        return "searchPage";
+    }
+
+    @RequestMapping(value = "/result")
+    public String hello(@RequestParam String search, Model model){
+        SearchResults searchResults = twitter.searchOperations().search(search);
+        List<Tweet> tweets = searchResults.getTweets();
+        model.addAttribute("tweets", tweets);
+        model.addAttribute("search", search);
+
+        return "resultPage";
+    }
+
+    @RequestMapping(value = "/postSearch", method = RequestMethod.POST)
+    public String postSearch(HttpServletRequest request, RedirectAttributes redirectAttributes){
+        String search = request.getParameter("search");
+        if(search.toLowerCase().contains("śmieci")){
+            redirectAttributes.addFlashAttribute("error", "Spróbuj wpisać Trump!");
+            return "redirect:/";
+        }
+        redirectAttributes.addAttribute("search", search);
+        return "redirect:result";
+    }
+}
